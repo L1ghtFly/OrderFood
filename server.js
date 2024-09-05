2dbb6707887cad4596539a8d4f812c8d84314fa1
L1ghtFly <edvard230807@gmail.com>
2024-09-05 21:49:12 +0200

    New page

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000; // Установите порт
+const PORT = 3000;
 
-// Тут подключение маршрутов
-const userRoutes = require('./routes/userRoutes');
-app.use('/api', userRoutes);
+// Импорт клиента MongoDB
+const client = require('./database');
+
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+    const pingResult = await client.db("admin").command({ ping: 1 });
+    res.send('Database connected successfully!');
+    await client.close();
+  } catch (error) {
+    res.status(500).send('Failed to connect to the database');
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
