dfc7fded5008200f46ac8d91fb4df33cc071c682
L1ghtFly <edvard230807@gmail.com>
2024-09-06 16:55:50 +0200

    New page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // Импорт клиента MongoDB
-const client = require('./database');
+const client = require('database');
 
 app.get('/', async (req, res) => {
   try {
--- userRoutes.js
+++ userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser } = require('../userController');
+const { createUser } = require('userController');
 
 router.post('/users', async (req, res) => {
     try {
