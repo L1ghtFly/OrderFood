ec8f6fd084a5f082fd457a8c29d3c0776a55a900
L1ghtFly <edvard230807@gmail.com>
2024-09-06 17:18:53 +0200

    New page

--- database.js
+++ database.js
@@ -9,16 +9,4 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
-
 module.exports = client;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const mongodbClient = require('./database');
 const app = express();
 const PORT = 3000;
 
-// Импорт клиента MongoDB
-const client = require('database.js');
-
 app.get('/', async (req, res) => {
   try {
-    await client.connect();
-    const pingResult = await client.db("admin").command({ ping: 1 });
-    res.send('Database connected successfully!');
-    await client.close();
+    await mongodbClient.connect();
+    const pingResult = await mongodbClient.db("admin").command({ ping: 1 });
+    res.send('Database connected successfully: ' + JSON.stringify(pingResult));
   } catch (error) {
+    console.error('Failed to connect to the database:', error);
     res.status(500).send('Failed to connect to the database');
+  } finally {
+    await mongodbClient.close();
   }
 });
 
